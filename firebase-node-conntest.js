d0fbbb319fa3dede3eb0b40358dbc846c23c1e58
ceh6yu <Sophia-Q28>
2015-06-03 15:47:32 +0200

    node request connection test (troublesouting firebase init command, which uses request as http client library)

---
+++ firebase-node-conntest.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ceh6yu on 03.06.2015.
+ */
+//npm install -g request
+
+
+var request = require('request')
+    , rand = Math.floor(Math.random()*100000000).toString()
+    ;
+request(
+    { method: 'PUT'
+        , uri: 'http://mikeal.iriscouch.com/testjs/' + rand
+        , multipart:
+        [ { 'content-type': 'application/json'
+            ,  body: JSON.stringify({foo: 'bar', _attachments: {'message.txt': {follows: true, length: 18, 'content_type': 'text/plain' }}})
+        }
+            , { body: 'I am an attachment' }
+        ]
+    }
+    , function (error, response, body) {
+        if(response.statusCode == 201){
+            console.log('document saved as: http://mikeal.iriscouch.com/testjs/'+ rand)
+        } else {
+            console.log('error: '+ response.statusCode)
+            console.log(body)
+        }
+    }
+)
